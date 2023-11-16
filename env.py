9ee3d256b606b7986b6d9c0cf98c4ccf39a8ca90
prwilkin <prwilkin@buffalo.edu>
2023-11-16 18:32:26 -0500

    issue with distance split

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import networkx as nx
+import matplotlib as mp
 
 # Load dataset
 file = './data.xlsx'
@@ -16,7 +17,8 @@ def generate(datafile, graph):
         # Parse lists
         connected_cities = row['connected cities'].split(',')
         connected_states = row['connected cities states'].split(',')
-        distances = [int(dist) for dist in row['distance to city'].split(',')]
+        distances = row['distance to city'].split(',')
+        # distances = [int(dist) for dist in row['distance to city'].split(',')]
 
         # Add the current city if not already in graph or update with lat, long, & interstates
         graph.add_node(node_name, lat=row['lat'], long=row['long'], interstates=row['interstates'])
@@ -27,14 +29,25 @@ def generate(datafile, graph):
             if connected_city_state not in graph:
                 graph.add_node(connected_city_state)
             # Distance is cost
-            graph.add_edge(node_name, connected_city_state, distance=distances[i])
+            graph.add_edge(node_name, connected_city_state, distance=int(distances[i]))
 
 
 generate(data, G)
 
+# nx.draw(G)
+# mp.pyplot.show()
+
 nodes = G.number_of_nodes()
 edges = G.number_of_edges()
 print("nodes: ", nodes, " | edges: ", edges)
-# print(G)
+# print(G.edges.data())
+# for node in G.nodes.data():
+#     print(node)
+    # print(node[2]["distance"])
+    # x = node[2]
+    # print(x['distance'])
+    # print(node[2])
+# for edge in nx.edges(G):
+#     print(edge)
 # for line in nx.generate_adjlist(G):
 #     print(line)
